8e7bbcfe0209ab418fad1ad50c7f657ca6e93d5f
xetiro <rubengeraldes@gmail.com>
2021-08-08 17:16:09 +0900

    Added a Singleton to represent the text corpus from where we extrat the words to insert on the Trie.

---
+++ src/main/kotlin/TextCorpus.kt
@@ -0,0 +1,12 @@
+object TextCorpus {
+
+    fun extractWords(): List<String> {
+        val words = content.split("\\s+".toRegex()).map { word ->
+            word.replace("""^[,\.]|[,\.]$""".toRegex(), "")
+        }
+        return words;
+    }
+
+    // Do insert a concatenation of text here and use it to load on the trie
+    private const val content: String = ""
+}
