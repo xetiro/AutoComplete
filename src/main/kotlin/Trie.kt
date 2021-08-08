cd7e49e8663794a487237a6266f59e3d6bf588b2
xetiro <rubengeraldes@gmail.com>
2021-08-08 17:00:37 +0900

    Updated the Trie.Node interface to include a boolean if this node marks a word and a counter variable to keep track of the number of words.

--- src/main/kotlin/Trie.kt
+++ src/main/kotlin/Trie.kt
@@ -1,15 +1,18 @@
 interface Trie {
+    val root: Node
+    fun insert(word: String)
+    fun containsWord(word: String): Boolean
+    fun autoComplete(word: String): List<String>
+
     interface Node {
-        val char: Char?
+        val char: Char
         val nodes: HashMap<Char, Node>
+        var isWord: Boolean
+        var count: Int
 
         fun addNode(char: Char)
         fun getNode(char: Char): Node
         fun getAllNodes(): List<Node>
         fun containsChar(ch: Char): Boolean
     }
-    val root: Node
-    fun insert(word: String)
-    fun containsWord(word: String): Boolean
-    fun autoComplete(word: String): List<String>
 }
