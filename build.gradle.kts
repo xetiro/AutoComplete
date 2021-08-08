e79461a1e6b963ca2034c217223af701a91307cf
xetiro <rubengeraldes@gmail.com>
2021-08-08 12:18:25 +0900

    Initial commit. Empty Kotlin/JS project.

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("js") version "1.5.10"
+}
+
+group = "me.xetiro"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
