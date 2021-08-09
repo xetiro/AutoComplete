e634ebf6cfcad0007b5cdb342c0e41f82f4bc25b
xetiro <rubengeraldes@gmail.com>
2021-08-09 18:33:10 +0900

    Updated the UI of the input box to open the text area only when the user types something.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,23 @@
+import kotlinx.browser.document
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLTextAreaElement
+
 fun main() {
     console.log("Hello, ${greet()}")
+    val input = document.getElementById("input") as HTMLInputElement
+    val result = document.getElementById("result") as HTMLTextAreaElement
+
+    input.addEventListener("input", {
+        if(input.value.isEmpty()) {
+            result.style.display = "none"
+            input.classList.remove("input-not-empty")
+            input.classList.add("input-empty")
+        } else {
+            result.style.display = "block"
+            input.classList.remove("input-empty")
+            input.classList.add("input-not-empty")
+        }
+    })
 }
 
 fun greet() = "world"
